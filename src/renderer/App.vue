a72ea84f2f5b97936b5ebe70073b2688b66dc7ee
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2022-05-03 22:16:01 -0300

    primeiro commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'kafka-simple-client'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
